18ff9c159b09c2921dce62692b88688af8509bf6
Noverd/Gagarinten <dasfasru@gmail.com>
2023-01-19 16:35:39 +0300

    0.7 Update Added classroom.py

---
+++ scratchlib/api/classroom.py
@@ -0,0 +1,17 @@
+import bs4
+import requests
+
+
+class ClassRoom:
+    def __init__(self, classroom_id: int):
+        self.id: int = classroom_id
+
+    def get_members_from_page(self, page: int = 1):
+        soup = bs4.BeautifulSoup(requests.get(f"https://scratch.mit.edu/classes/{self.id}/students/?page={page}").text,
+                                 "html"
+                                 ".parser")
+        return \
+            [_.find("span").contents[1].contents[0].strip() for _ in soup.find_all("li", {"class": "user thumb item"})]
+
+
+print(ClassRoom(358511).get_members_from_page())
